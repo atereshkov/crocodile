23b38a9e4ceb3b59e9b8c4382e48fb5c3deb38c4
Alexander Tereshkov <alexander@tereshkov.pw>
2020-01-06 21:12:57 +0300

    Added setCurrentScreen

--- lib/app/service/analytics/remote_analytics_service_type.dart
+++ lib/app/service/analytics/remote_analytics_service_type.dart
@@ -2,4 +2,5 @@ import 'package:crocodile_game/app/model/analytics_event_type.dart';
 
 abstract class RemoteAnalyticsServiceType {
   Future<void> sendAnalyticsEvent(AnalyticsEventType event);
+  void setCurrentScreen(String screenName);
 }
