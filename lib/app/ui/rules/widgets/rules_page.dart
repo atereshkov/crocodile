c14034ecfa8ea257f501aa4e564957046459f6b4
Alexander Tereshkov <alexander@tereshkov.pw>
2020-01-06 16:57:20 +0300

    Basic rules page UI

--- lib/app/ui/rules/widgets/rules_page.dart
+++ lib/app/ui/rules/widgets/rules_page.dart
@@ -36,7 +36,111 @@ class _RulesPageState extends State<RulesPage> {
   }
 
   Widget _buildBody(BuildContext context) {
-    return Column();
+    return Stack(
+      children: <Widget>[
+        _gradientBackground(),
+        Column(
+          children: <Widget>[
+            _buildSinglePlayRules(context),
+            _buildTeamPlayRules(context),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSinglePlayRules(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 14, vertical: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Icon(
+                Icons.person,
+                color: Colors.white,
+              ),
+              Padding(padding: EdgeInsets.only(left: 8)),
+              Icon(
+                Icons.person,
+                color: Colors.white,
+              ),
+              Padding(padding: EdgeInsets.only(left: 12)),
+              Text(
+                "Обычная игра",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+          Padding(padding: EdgeInsets.only(top: 12)),
+          Text(
+            "Игрок должен изобразить слово, используя только жесты и мимику (говорить нельзя), а остальные игроки должны угадать это слово. Тот, кто угадал - занимает место ведущего игрока и игра продолжается.",
+            style: TextStyle(
+              fontSize: 15,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTeamPlayRules(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 14, vertical: 0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Icon(
+                Icons.people,
+                color: Colors.white,
+              ),
+              Padding(padding: EdgeInsets.only(left: 8)),
+              Icon(
+                Icons.people,
+                color: Colors.white,
+              ),
+              Padding(padding: EdgeInsets.only(left: 12)),
+              Text(
+                "Командная игра",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+          Padding(padding: EdgeInsets.only(top: 12)),
+          Text(
+            "Игроки делятся на команды с произвольным количеством игроков. Далее команды по кругу показывают слова (один игрок от команды). Игрок показывает слово и только его команда пытается отгадать слово. Если слово отгадано за отведенное время, команде начисляются баллы и очередь переходит к следущей команде по кругу.",
+            style: TextStyle(
+              fontSize: 15,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _gradientBackground() {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [Color(0xB300A010), Color(0xFFF2E6E6)]
+        )
+      ),
+    );
   }
 
 }
