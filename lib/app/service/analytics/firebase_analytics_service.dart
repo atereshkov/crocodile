25dc64cbd62fa92386d3b8771a3f2ed92af3355f
Alexander Tereshkov <alexander@tereshkov.pw>
2020-01-19 22:42:35 +0300

    Only send firebase analytics in release mode

--- lib/app/service/analytics/firebase_analytics_service.dart
+++ lib/app/service/analytics/firebase_analytics_service.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:flutter/foundation.dart';
 
 import 'package:crocodile_game/app/service/analytics/remote_analytics_service_type.dart';
 import 'package:crocodile_game/app/model/analytics_event_type.dart';
@@ -8,14 +9,18 @@ class FirebaseAnalyticsService implements RemoteAnalyticsServiceType {
   final FirebaseAnalytics analytics = FirebaseAnalytics();
 
   Future<void> sendAnalyticsEvent(AnalyticsEventType event) async {
-    await analytics.logEvent(
-      name: event.name,
-      parameters: event.parameters,
-    );
+    if (kReleaseMode) {
+      await analytics.logEvent(
+        name: event.name,
+        parameters: event.parameters,
+      );
+    }
   }
 
   void setCurrentScreen(String screenName) {
-    analytics.setCurrentScreen(screenName: screenName);
+    if (kReleaseMode) {
+      analytics.setCurrentScreen(screenName: screenName);
+    }
   }
 
 }
